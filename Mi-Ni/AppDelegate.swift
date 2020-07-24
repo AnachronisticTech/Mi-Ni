4a891b30e33fdd0bbe32f02e66a428b66807ee3a
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2020-07-24 17:37:34 +0300

    Initial Commit

---
+++ Mi-Ni/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mi-Ni
+//
+//  Created by Daniel Marriner on 24/07/2020.
+//  Copyright © 2020 Daniel Marriner. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
